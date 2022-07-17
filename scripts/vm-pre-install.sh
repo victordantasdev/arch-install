33ad87ba70cc5bd147aaae8a36f0c34d809d8772
victordantasdev <victordantasdeveloper@gmail.com>
2022-07-17 11:44:12 -0300

    fix: adds vm pre-install script

---
+++ scripts/vm-pre-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+loadkeys br-abnt2
+
+mkfs.fat -F32 /dev/vda1
+mkfs.ext4 /dev/vda2
+mkfs.ext4 /dev/vda3
+
+mount /dev/vda2 /mnt
+mkdir -p /mnt/boot/efi /mnt/home
+mount /dev/vda2 /mnt/boot/efi
+mount /dev/vda3 /mnt/home
+
+genfstab -U -p /mnt >>/mnt/etc/fstab
+arch-chroot /mnt
