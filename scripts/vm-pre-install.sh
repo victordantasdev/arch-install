598a16bd54e3ed6ddaea5b35f761bb48b34c252f
victordantasdev <victordantasdeveloper@gmail.com>
2022-07-17 12:06:29 -0300

    feat: update vm pre-install script

--- scripts/vm-pre-install.sh
+++ scripts/vm-pre-install.sh
@@ -11,5 +11,7 @@ mkdir -p /mnt/boot/efi /mnt/home
 mount /dev/vda2 /mnt/boot/efi
 mount /dev/vda3 /mnt/home
 
+pacstrap /mnt base base-devel linux linux-firmware nano vim dhcpcd git wget curl
+
 genfstab -U -p /mnt >>/mnt/etc/fstab
 arch-chroot /mnt
